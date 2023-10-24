8eacc69ba634934cc5d28add0d544568de97705e
NobleFoley <noafoley@ucsc.edu>
2023-10-24 00:31:55 -0700

    Course Listing with creation and fetching from db

--- frontend/flashcards/src/components/MyCourses.js
+++ frontend/flashcards/src/components/MyCourses.js
@@ -1,40 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { db } from "./firebase_config";
 import "./MyCourses.css";
 
 function MyCourses() {
-  const courses = [
-    {
-      name: "Introduction to Programming",
-      description:
-        "This course introduces the fundamental concepts of programming. Students will learn to write simple programs using variables, loops, and conditionals.",
-    },
-    {
-      name: "Web Development",
-      description:
-        "This course covers the basics of web development, including HTML, CSS, and JavaScript. Students will learn to create interactive websites.",
-    },
-    {
-      name: "Data Structures and Algorithms",
-      description:
-        "This course provides an overview of data structures like arrays, linked lists, stacks, queues, trees, and graphs. It also covers basic algorithms for searching and sorting data.",
-    },
-    {
-      name: "Database Systems",
-      description:
-        "This course introduces the principles of database systems. Topics include data modeling, relational databases, SQL, and database design.",
-    },
-    {
-      name: "Machine Learning",
-      description:
-        "This course provides an introduction to machine learning techniques and algorithms. Students will learn about regression, classification, clustering, and neural networks.",
-    },
-  ];
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "courses"), (snapshot) => {
+      const newCourses = snapshot.docs.map((doc) => doc.data());
+      setCourses(newCourses);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const breakAll = (str) => {
+    const words = str.split(" ");
+
+    // Check each word
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > 12) {
+        return true;
+      }
+    }
+
+    return false;
+  };
 
   return (
     <div className="Course-wrapper">
       <ul className="scrollable-container">
         {courses.map((item, index) => (
-          <li key={index} className="item">
+          <li
+            key={index}
+            className={`item ${breakAll(item.name) ? "break-all" : ""}`}
+          >
             <h1 className="Course-name">{item.name}</h1>
             <p className="Course-description">{item.description}</p>
           </li>
--- frontend/flashcards/src/pages/NewCourse.js
+++ frontend/flashcards/src/pages/NewCourse.js
@@ -1,10 +1,90 @@
-import React from "react";
-import './NewCourse.css'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { db } from "../components/firebase_config";
+import { collection, addDoc } from "firebase/firestore";
+import { BsFillExclamationSquareFill } from "react-icons/bs";
+import Navbar from "../components/Navbar";
+import "./NewCourse.css";
 
 function NewCourse() {
-  return <div className="NewCourse">
-    Create a New Course
-  </div>;
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [textAreaDisabled, setTextAreaDisabled] = useState(false);
+  const [inputDisabled, setInputDisabled] = useState(false);
+  const [noName, setNoName] = useState(false);
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    const newName = event.target.value;
+    if (newName.length <= 50) {
+      setName(newName);
+    } else {
+      setInputDisabled(!inputDisabled);
+    }
+  };
+
+  const handleDescriptionChange = (event) => {
+    const newDescription = event.target.value;
+    if (newDescription.length <= 250) {
+      setDescription(newDescription);
+    } else {
+      setTextAreaDisabled(!textAreaDisabled);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    if (name.length <= 0) {
+      return setNoName(true);
+    }
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, "courses"), {
+        name: name,
+        description: description,
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    navigate("/");
+  };
+
+  return (
+    <div className="course-page-wrapper">
+      <Navbar />
+      <div className="NewCourse">
+        <h1 className="new-course-header">Create a New Course</h1>
+        <input
+          className="course-name-input"
+          placeholder="Enter name..."
+          value={name}
+          onChange={handleInputChange}
+          required
+        />
+        <p className="char-count">{name.length}/50</p>
+        {noName ? (
+          <div className="noName-error">
+            <BsFillExclamationSquareFill />
+            <p className="noName-text">Please enter a name...</p>
+          </div>
+        ) : null}
+        <textarea
+          value={description}
+          onChange={handleDescriptionChange}
+          className="course-description"
+          placeholder="Enter description..."
+        ></textarea>
+        <p className="char-count">{description.length}/250</p>
+        <div className="button-wrap">
+          <button className="course-save" onClick={handleSubmit}>
+            Save
+          </button>
+          <Link to="/">
+            <button className="course-cancel">Cancel</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NewCourse;
