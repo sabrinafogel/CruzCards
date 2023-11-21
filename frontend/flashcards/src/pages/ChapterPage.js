e4eb860c02f8c5133f9c58553f43982e0d1aae18
TKJ <josephkteresa@gmail.com>
2023-11-20 21:35:22 -0800

    search for sets
    based on serverdummy

--- frontend/flashcards/src/pages/ChapterPage.js
+++ frontend/flashcards/src/pages/ChapterPage.js
@@ -23,6 +23,9 @@ function ChapterPage() {
 
   const [isEditor, setIsEditor] = useState(false);
 
+  var search = "";
+  const [searchChapters, setSearchChapters] = useState([]);
+
   // Will fetch course from db and will fetch again if courseid changes
   useEffect(() => {
     // Fetches the course info from the backend
@@ -57,12 +60,28 @@ function ChapterPage() {
 
         // Will keep a local copy of the sets for removing sets without extra reads and refreshes
         setSets(course_info.chapters[chapterIndex].sets);
+        setSearchChapters(course_info.chapters[chapterIndex].sets);
       } catch (error) {
         console.error("Error:", error);
       }
     };
     fetchCourseInfo();
   }, [courseid, chapterIndex, user]);
+
+  const searchFeature = async(e) => {
+    search = e.target.value.trim();
+    if (search === ""){
+      setSearchChapters(sets);
+      return;
+    }
+    var newSets = [];
+    for (let i = 0; i < sets.length; i++){
+      if ((sets[i].name.toLowerCase()).startsWith(search.toLowerCase())){
+        newSets.push(sets[i]);
+      }
+    }
+    setSearchChapters(newSets);
+  }
 
   // Returns a loading screen if the fetch hasn't finished yet
   if (!course_info || !course_info.chapters) {
@@ -174,7 +193,7 @@ function ChapterPage() {
           </h1>
           {/* Search to look through your sets (NOT IMPLEMENTED) */}
           <div className="input-wrapper">
-            <input className="search-input" placeholder="Search" />
+            <input className="search-input" placeholder="Search" onChange={searchFeature}/>
           </div>
         </div>
         {course_info && currentChapter.description ? (
@@ -199,7 +218,7 @@ function ChapterPage() {
       <div className="setDisplay">
         <ul>
           {/* Maps all of the sets with this format */}
-          {sets?.map((set, setIndex) => (
+          {searchChapters?.map((set, setIndex) => (
             <div className="set-wrapper">
               {/* Link to EditSetPage */}
               <Link
