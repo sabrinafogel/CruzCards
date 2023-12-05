e552f025837151e27ed3a8195c367790c939a601
Sabrina Fogel <sabrinafogel20@gmail.com>
2023-12-04 17:49:11 -0800

    Added documentation for AccountDropDown.js

--- frontend/flashcards/src/components/AccountDropDown.js
+++ frontend/flashcards/src/components/AccountDropDown.js
@@ -3,9 +3,22 @@ import "./AccountDropDown.css";
 import { UserAuth } from "./AuthContext";
 import { Link } from "react-router-dom";
 
+/**
+ * AccountDropDown.js
+ * AccountDropDown() displays the user's account dropdown field, which can either display:
+ * 1. If the user is not logged in, the drop down will display a sign-in link
+ * 2. If the user is logged in, the drop down will display a link to the profile section and a sign-out link
+ * @returns None
+ */
 function AccountDropDown() {
+  // Uses the UserAuth function from components/AuthContext to create user and logOut
   const { user, logOut } = UserAuth();
 
+  /**
+   * AccountDropDown.js
+   * handleSignOut() is an asynchronous function that invokes the logOut() function from UserAuth()
+   * @returns None
+   */
   const handleSignOut = async () => {
     try {
       await logOut();
@@ -14,6 +27,7 @@ function AccountDropDown() {
     }
   };
 
+  // Return a list of links/buttons as described in the AccountDropDown() DocString
   return (
     <ul className="List">
       {user === null ? (
