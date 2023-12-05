f1970913be9a02b5df8ebd093ae2053dc291972b
Sabrina Fogel <sabrinafogel20@gmail.com>
2023-12-04 23:13:09 -0800

    Added documentation for CoursePage.js

--- frontend/flashcards/src/pages/CoursePage.js
+++ frontend/flashcards/src/pages/CoursePage.js
@@ -7,22 +7,45 @@ import { FaPlus, FaAngleLeft } from "react-icons/fa6";
 import { AiFillEdit } from "react-icons/ai";
 import { FaAngleRight, FaCheck } from "react-icons/fa";
 
+/**
+ * CoursePage.js
+ * CoursePage() displays the Chapters associated with a given Course.
+ * Additionally, ChapterPage() displays different options if the user 
+ * has editing permissions for the Course (if they do, they can add Chapters to the Course)
+ * @returns CoursePage, a webpage that displays the Chapters associated with a given Course
+ */
 function CoursePage() {
+  
+  // User from authcontext provider this will be used later for restricting non-editors from changing the sets
   const { user } = UserAuth();
 
+  // Gets these params from the url
   const { courseid } = useParams();
+  // Used to keep the course_info and sets for use on the page
   const [course_info, setCourseInfo] = useState([]);
 
+  // Initializes isEditor, which contains information about if a user has editing permissions
   const [isEditor, setIsEditor] = useState(false);
 
+  // Initializes toggle
   const [toggle, setToggle] = useState(undefined);
 
+  // Initializes variables used for searching through courses 
+  // (searchChapters and setSearchChapters using React's useState hook)
   var search = "";
   const [searchChapters, setSearchChapters] = useState([]);
 
+  // Will fetch course from db and will fetch again if courseid changes
   useEffect(() => {
+
+    /**
+     * CoursePage.js
+     * fetchCourseInfo() is an asynchronous function that fetches course information
+     * from the backend based on a given courseID
+     */
     const fetchCourseInfo = async () => {
       try {
+        // Encodes the courseid in the url for fetching
         const response = await fetch(
           `http://localhost:8080/courseinfo?courseid=${encodeURIComponent(
             courseid
@@ -33,20 +56,27 @@ function CoursePage() {
           throw new Error("Network response was not ok");
         }
 
+        // Parse response as JSON
         const course_info = await response.json();
+
+        // Sets the course_info for future use, based on the parsed JSON response
         setCourseInfo(course_info);
         setSearchChapters(course_info.chapters);
         setCourseNameInput(course_info.name);
+
         if (course_info.privacy !== 'undefined'){
           setToggle(course_info.privacy);
         }
 
+        // Set the correct information about those with editing permissions of the course
+        // Including the owner and any editors
         const course_editors = [course_info.owner];
 
         course_info.editors.forEach((editor) => {
           course_editors.push(editor);
         });
 
+        // If the user's email is found within course_editors, give them editor permissions
         if (course_editors.includes(user.email)) {
           setIsEditor(true);
         } else {
@@ -56,16 +86,27 @@ function CoursePage() {
         console.error("Error:", error);
       }
     };
+
+    // Call fetchCourseInfo()
     fetchCourseInfo();
-  }, [courseid, user]);
+  }, [courseid, user]); // Dependency array includes the courseid and user
 
   const chapters = course_info.chapters;
 
   
-  // Search feature
+  /**
+   * CoursePage.js
+   * searchFeature() is an asynchronous function that contains the search feature for Courses
+   * By taking in an event's target value and using that value to filter through Courses
+   * @param {event} e 
+   * @returns None
+   */
   const searchFeature = async (e) => {
-    //setSearch(e.target.value);
+
+    // Grab the value from e, perform modifications (trim), and store in search
     search = e.target.value.trim();
+    
+    // If the search string is empty, set the searched chapters to the original list and return
     if (search === "") {
       setSearchChapters(chapters);
       return;
@@ -82,46 +123,87 @@ function CoursePage() {
         throw new Error("Network response was not ok");
       }
 
+      // searchChapters is the JSON response to the fetch request
       const searchChapters = await response.json();
+
+      // set searchChapters accordingly
       setSearchChapters(searchChapters.chapters);
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
+  // Initializes variables for tracking any changes in Course Name
   const [isChangingName, setIsChangingName] = useState(false);
   const [courseNameInput, setCourseNameInput] = useState(course_info.name);
 
+  // Initializes variables for tracking any changes in Course Description
   const [isChangingDesc, setIsChangingDesc] = useState(false);
   const [courseDescInput, setCourseDescInput] = useState(
     course_info.description
   );
 
+  /**
+   * CoursePage.js
+   * handleCourseNameChange() is a method that changes the course's name to an event target's value
+   * @param {event} e 
+   * @returns None
+   */
   const handleCourseNameChange = (e) => {
     setCourseNameInput(e.target.value);
   };
 
+  /**
+   * CoursePage.js
+   * handleCourseDescChange() is a method that changes the course's description to an event target's value
+   * @param {event} e 
+   * @returns None
+   */
   const handleCourseDescChange = (e) => {
     setCourseDescInput(e.target.value);
   };
 
+  /**
+   * CoursePage.js
+   * handleJSONDownload() is a method that creates a downloadable file.
+   * This file contains JSON information representing a course
+   * @param {event} e 
+   * @returns None
+   */
   const handleJSONDownload = (e) => {
+
+    // Convert the course_info object into a JSON string, then into a Blob
     const courseJSONData = JSON.stringify(course_info);
     const blob = new Blob([courseJSONData], { type: "application/json" });
+    
+    // Create a link element for download
     const link = document.createElement("a");
     link.download = `${course_info.name}.json`;
     link.href = window.URL.createObjectURL(blob);
+    
+    // Add the link to the CoursePage
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
+  /**
+   * CoursePage.js
+   * handleNameChangeSubmit() is a function that takes care of the changes to a name and description of a course
+   * @returns None
+   */
   const handleNameChangeSubmit = async (e) => {
+
+    // Set the modes for changing name and description to false
     setIsChangingName(false);
     setIsChangingDesc(false);
+
+    // If there is no input in courseName, return immediately
     if (courseNameInput.length === 0) {
       return;
     }
+
+    // Send a POST request to the server to update course information
     const response = await fetch("http://localhost:8080/editCourse", {
       method: "POST",
       headers: {
@@ -138,13 +220,20 @@ function CoursePage() {
       throw new Error("Network response was not ok");
     }
 
+    // Get and store the new course information (name and description)
     const newCourseInfo = { ...course_info };
     newCourseInfo.name = courseNameInput;
     newCourseInfo.description = courseDescInput;
 
+    // Modify the course to relflect the new information
     setCourseInfo(newCourseInfo);
   };
 
+  /**
+   * CoursePage.js
+   * toggleButton() is a function that handles toggling the privacy state of a course
+   * @returns None
+   */
   const toggleButton = async() => {
     //setToggle(!toggle);
 
@@ -163,14 +252,20 @@ function CoursePage() {
       throw new Error("Network response was not ok");
     }
 
+    // Create a variable newCourseInfo that stores the course_info
     const newCourseInfo = {...course_info};
+
+    // Change newCourseInfo's privacy setting
     newCourseInfo.privacy = !toggle;
+
+    // Modify the course to reflect the new information
     setCourseInfo(newCourseInfo);
 
     setToggle(!toggle);
 
   }
 
+  // Return the formatted CoursePage
   return (
     <div>
       <Navbar />
