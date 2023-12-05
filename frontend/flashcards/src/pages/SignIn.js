ef8f79284faa8c6ef5139cf6416f0205a776c7d4
Sabrina Fogel <sabrinafogel20@gmail.com>
2023-12-05 01:30:31 -0800

    Added documentation to SignIn.js

--- frontend/flashcards/src/pages/SignIn.js
+++ frontend/flashcards/src/pages/SignIn.js
@@ -4,7 +4,14 @@ import { UserAuth } from "../components/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import "./SignIn.css";
 
+/**
+ * SignIn.js
+ * SignIn() displays the webpage for signing in as a returning user
+ * @returns Sign In User page
+ */
 const SignIn = () => {
+
+  // Initialize necessary variables
   const { user, googleSignIn, emailSignIn } = UserAuth();
   const navigate = useNavigate();
 
@@ -13,21 +20,37 @@ const SignIn = () => {
     input2: "",
   });
 
+  /**
+   * SignIn.js
+   * handleInputChange() is a method that changes the input values to an event target's value
+   * @param {event} e 
+   * @returns None
+   */
   const handleInputChange = (e) => {
     const value = e.target.value;
-    console.log(value);
+    //console.log(value);
     setinputvalues({
       ...inputvalues,
       [e.target.name]: value,
     });
   };
 
+  /**
+   * SignIn.js
+   * handleSubmit() is an asynchronous function that enters user information 
+   * @param {event} e
+   */
   const handleSubmit = (e) => {
     e.preventDefault();
     emailSignIn(inputvalues.input1, inputvalues.input2);
-    console.log(inputvalues);
+    // console.log(inputvalues);
   };
 
+  /**
+   * SignIn.js
+   * handleGoogleSignIn() is an asynchronous function that uses Google's authentication to sign the user in
+   * @param {event} e
+   */
   const handleGoogleSignIn = async () => {
     try {
       await googleSignIn();
@@ -37,9 +60,12 @@ const SignIn = () => {
   };
   useEffect(() => {
     if (user != null) {
+      // Navigate to the homepage
       navigate("/");
     }
   }, [user, navigate]);
+
+  // Return the Sign In page
   return (
     <div className="page-wrapper">
       <div className="Sign-in-container">
